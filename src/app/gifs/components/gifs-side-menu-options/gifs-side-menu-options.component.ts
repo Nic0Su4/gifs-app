bb1680c17974b4ad68aa6528b13570734d4b3b9a
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-09-09 00:18:13 -0500

    feat: enhance side menu with dynamic menu options and update index.html structure

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent {}
+export class GifsSideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs Populares',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      subLabel: 'Buscar Gifs',
+    },
+  ];
+}
