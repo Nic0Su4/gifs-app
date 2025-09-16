29a404d277828a5abed8691fce2bf165da8421c8
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-09-15 22:36:42 -0500

    feat: implement search functionality for GIFs with user input handling

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interface';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
@@ -30,5 +31,27 @@ export class GifsService {
         this.trendingGifs.set(gifs);
         this.trendingGifsLoading.set(false);
       });
+  }
+
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          q: query,
+          limit: '20',
+          rating: 'r',
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+
+        // TODO Historial
+      );
+    // .subscribe((resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+    //   console.log({ search: gifs });
+    // });
   }
 }
