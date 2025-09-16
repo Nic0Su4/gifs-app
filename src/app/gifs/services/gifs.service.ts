d0f8aacb19b8a5ba63d63db6585f2055b62ada49
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-09-15 22:07:06 -0500

    feat: implement Giphy API integration and enhance GIF handling with new interfaces and services

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment';
+import type { GiphyResponse } from '../interfaces/giphy.interface';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+  private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal<boolean>(true);
+
+  constructor() {
+    this.loadTrendingGifs();
+  }
+
+  loadTrendingGifs() {
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: '20',
+          rating: 'r',
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
+      });
+  }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,8 @@ export const environment = {
   companyName: 'Gifs',
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  giphyApiKey: 'sPBULJ9AQ6dCV1lKR77RPKznlO6UqLGr',
+  giphyUrl: 'https://api.giphy.com/v1',
 };
