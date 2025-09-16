6e36b04a4f9d462209fe22982c6d4fd9a7fd5d7e
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-09-15 23:14:40 -0500

    feat: add GIF history route and component to display search history

--- src/app/gifs/pages/gif-history/gif-history.component.ts
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gif-history',
   imports: [],
   templateUrl: './gif-history.component.html',
 })
-export default class GifHistoryComponent {}
+export default class GifHistoryComponent {
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(map((params) => params['query']))
+  );
+}
