d685daedbec871e92bd9ff37f77bb3a4b88190c6
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2026-01-05 23:20:06 -0500

    feat: Add trending GIFs page with infinite scroll, Giphy API integration for grouped display, and local storage-backed search history.

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,12 +1,26 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core';
+// import { GifListComponent } from '../../components/gif-list/gif-list.component';
 import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
+  // imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   gifsService = inject(GifsService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
+
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    console.log({ scrollTop, clientHeight, scrollHeight })
+  }
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -19,6 +19,15 @@ export class GifsService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal<boolean>(true);
 
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+
+    return groups;
+  });
+
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
